0c65e34254df095843733b57ce93b33da3b207a6
Kyle Bennett <105824105+kylebennett95@users.noreply.github.com>
2022-09-19 18:10:50 -0500

    Add files via upload

---
+++ functions-students/main.js
@@ -0,0 +1,98 @@
+const buyClay = () => {
+    const clay = {
+
+    }
+    console.log(clay)
+}
+
+
+const makePottery = () => {
+    const test = buyClay.clay.shape('bowl')
+}
+//console.log(buyClay)
+const bisqueFire = () => {
+
+}
+
+const glazePotter = () => {
+
+}
+
+
+//const fillGasTank = (gallons) => {
+    if (gallons > 15) {
+        //console.log(`You cannot add this much gas to the tank`)
+    }
+    else {
+        //console.log(`I filled the tank with ${gallons} of gas`)
+    }
+//}
+
+//fillGasTank(15)
+//fillGasTank(22)
+//fillGasTank(14)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Lynn JavaScript function notes
+//function myFunction() {
+//}
+
+// parameters = arguments. These are what are going into the function. A function should only do one thing. 
+//If you need to do two things create two functions
+//const myFunction = () => {
+    //console.log(monkey)
+    ///return "I'm a function" //after a return command, the computer stops reading the command
+//}
+//myFunction("banana") this is called invoking
+
+// feed cats
+//make tacos
+//wash dishes
+//put away dishes
+
+//const FeedCats = () => {
+  //  return "cats fed"
+//}
+
+//const areCatsFed = FeedCats()
+//console.log(areCatsFed)
+
+//const makeTacos = () => {
+  //  return "tacos yummy"
+//}
+
+//const doChores = () => {
+  //  const fedCats = FeedCats()
+    //let taco = "no taco"
+    //if(fedCats === "cats fed") {
+      //  taco = makeTacos()
+    //}
+    //return taco
+//}
+
+//const chores = doChores()
+//console.log(chores)
+
+//      SCOPE NOTES
+
+//const temp = 105
+//const predictTemperature = () => {
+  //  return temp - 20
+//}
+
+//console.log(temp)
+
+//local scope- within function. block scope- within block of code like curly brackets {}
+
